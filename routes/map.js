546259a5a064e6ec5ac4549e0d8ea8fdd337048f
ADDad <add.twitch@rotarydial.com>
2017-09-24 22:37:36 -0400

    Using geocoder instead of the Google Maps API directly in the root route

--- routes/map.js
+++ routes/map.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var googlemaps = require('googlemaps');
 var router = express.Router();
+var googlemaps = require('googlemaps');
+var geocoder = require('geocoder');
 
 var publicConfig = {
     key: 'AIzaSyBXDUQZ7EOVrbsWA2ee5ORX7U0MrbC3DME',
@@ -14,7 +15,25 @@ var gmAPI = new googlemaps(publicConfig);
 
 /* Show a map! */
 router.get('/', function(req, res, next) {
-    console.log('Starting some map stuff');
+    // geocoder - "My location"
+    var startingLocation = '530 Valley Rd, Montclair, NJ, USA';
+    geocoder.geocode(startingLocation, function(err, result) {
+
+        console.log("geocode results...");
+        var lat = result.results[0].geometry.location.lat;
+        var lng = result.results[0].geometry.location.lng;
+
+        console.log(startingLocation + ':');
+        console.log('  ├─ lattitude: ' + lat);
+        console.log('  └─ longitude: ' + lng);
+        console.log(result.results);
+
+        res.send(result);
+    });
+    
+});
+
+router.get('/gmapi', function(req, res, next) {
 
     // geocode API 
     var geocodeParams = {
