6e4376a9dcf29e66d981d13c5d056113dd3ec6de
ADDad <add.twitch@rotarydial.com>
2017-09-24 23:45:43 -0400

    GeoJSON formatted data

--- routes/map.js
+++ routes/map.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var googlemaps = require('googlemaps');
 var geocoder = require('geocoder');
+var geojson = require('geojson');
+
+var startingLocation ='530 Valley Rd, Montclair, NJ';
 
 var publicConfig = {
     key: 'AIzaSyBXDUQZ7EOVrbsWA2ee5ORX7U0MrbC3DME',
@@ -15,20 +18,78 @@ var gmAPI = new googlemaps(publicConfig);
 
 /* Show a map! */
 router.get('/', function(req, res, next) {
-    // geocoder - "My location"
-    var startingLocation = '530 Valley Rd, Montclair, NJ, USA';
     geocoder.geocode(startingLocation, function(err, result) {
 
-        console.log("geocode results...");
+        console.log('geocoder results...');
+        var street = startingLocation;
         var lat = result.results[0].geometry.location.lat;
         var lng = result.results[0].geometry.location.lng;
 
         console.log(startingLocation + ':');
         console.log('  ├─ lattitude: ' + lat);
         console.log('  └─ longitude: ' + lng);
-        console.log(result.results);
 
-        res.send(result);
+        // console.log('Unformatted:');
+        // console.log(result.results);
+
+        var data = { 
+            'name': 'My Location',
+            'category': 'User', 
+            'street': street, 
+            'lat': lat, 
+            'lng': lng 
+        };
+        
+        var geojsonResult = geojson.parse(data, 
+            { 
+                Point: ['lat', 'lng'],
+                include: ['name', 'category']
+            });
+
+        console.log('GeoJSON formatted:');
+        console.log(geojsonResult);
+
+        res.send(geojsonResult);
+    });
+    
+});
+
+router.get('/geojson', function(req, res, next) {
+    // test and tinker here
+    geocoder.geocode(startingLocation, function(err, result) {
+        
+        console.log('GeoJSON preformat:');
+        var street = result.results[0].address_components[1].short_name; //can't rely on this now, can we?
+        var lat = result.results[0].geometry.location.lat;
+        var lng = result.results[0].geometry.location.lng;
+
+        console.log(startingLocation + ':');
+        console.log('  ├─ street: ' + street);
+        console.log('  ├─ lattitude: ' + lat);
+        console.log('  └─ longitude: ' + lng);
+
+        // TODO: assemble this properly later
+        var data = [
+            { 
+                'name': 'My Location',
+                'category': 'User', 
+                'street': street, 
+                'lat': lat, 
+                'lng': lng 
+            }
+        ];
+
+        console.log('Sending this:');
+        console.log(data);
+
+        var geojsonResult = geojson.parse(data, 
+            { 
+                Point: ['lat', 'lng']
+            });
+
+        console.log(geojsonResult);
+
+        res.send(geojsonResult);
     });
     
 });
