90e117ead5243a1e045e7d231f91f4945dcb9154
Christopher Drew <github@rotarydial.com>
2017-10-03 23:28:49 -0400

    [WIP] Added /search route
     Changes to be committed:
    	modified:   app.js
    	new file:   public/images/searchicon.png
    	modified:   public/stylesheets/style.css
    	new file:   routes/search.js
    	new file:   views/search.ejs

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var map = require('./routes/map');
+var search = require('./routes/search');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/map', map);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
